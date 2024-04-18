8b2d50dd8d8fa44dc0489e8db020e8384ef46c07
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-04-18 18:55:54 +0400

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Starting Flask WEB application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+# Creating an instance of Flask class
+app = Flask('__name__')
+
+@app.route("/states_list", strict_slashes=False)
+def get_states():
+    """get states"""
+    states = list(storage.all(State).values())
+    #print(states)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """close storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
