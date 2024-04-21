fe4109339c1d4b3456d7987da2e7a08b370aba9b
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-04-21 19:06:37 +0400

    fix error

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,70 +1,74 @@
 #!/usr/bin/python3
-from sqlalchemy import create_engine
-from models.base_model import Base
-from sqlalchemy.orm import scoped_session, sessionmaker
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+"""Data base storage of HBNB"""
 from os import getenv
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship, scoped_session, sessionmaker
 
 
 class DBStorage:
     __engine = None
     __session = None
-    classes = ["Amenity", "City", "Place", "Review", "State", "User"]
 
     def __init__(self):
-        self.user = getenv("HBNB_MYSQL_USER")
-        self.password = getenv("HBNB_MYSQL_PWD")
-        self.host = getenv("HBNB_MYSQL_HOST", 'localhost')
-        self.database = getenv("HBNB_MYSQL_DB")
-
+        user = getenv("HBNB_MYSQL_USER")
+        password = getenv("HBNB_MYSQL_PWD")
+        host = getenv("HBNB_MYSQL_HOST")
+        database = getenv("HBNB_MYSQL_DB")
         self.__engine = create_engine(
             "mysql+mysqldb://{}:{}@{}/{}"
-            .format(self.user, self.password, self.host, self.database),
+            .format(user, password, host, database),
             pool_pre_ping=True,
         )
 
-        if getenv('HBNB_ENV') == 'test':
+        if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        objs = dict()
-
+        """Getting all values from db"""
         if cls is None:
-            for class_name in self.classes:
-                type_of_class = eval(class_name)
-                values = self.__session.query(type_of_class).all()
-                objs = {"{}.{}".format(
-                    type_of_class.__name__, value.id
-                    ): value for value in values}
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
         else:
-            values = self.__session.query(cls).all()
-            objs = {"{}.{}".format(
-                cls.__name__, value.id
-                ): value for value in values}
-
-        return objs
+            if isinstance(cls, str):
+                cls = eval(cls)
+            objs = self.__session.query(cls)
+        return {"{}.{}"
+                .format(type(obj).__name__, obj.id): obj for obj in objs}
 
     def new(self, obj):
+        """Create new row"""
         self.__session.add(obj)
 
     def save(self):
+        """Save changes"""
         self.__session.commit()
 
     def delete(self, obj=None):
+        """Delete row"""
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
+        """Reload from db"""
         Base.metadata.create_all(self.__engine)
-        session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session)
+        session_factory = sessionmaker(
+            bind=self.__engine,
+            expire_on_commit=False,
+        )
+        Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self):
+        """Close db"""
         self.__session.close()
