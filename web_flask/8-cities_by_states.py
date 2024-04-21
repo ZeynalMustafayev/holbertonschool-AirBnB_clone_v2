edce4746974c11ee023b8632a0e3f4a40b398bed
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-04-21 19:00:21 +0400

    fix error

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""
-Starting Flask WEB application
-"""
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /cities_by_states: HTML page with a list of all states and related cities.
+"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
+app = Flask("__name__", template_folder="web_flask/templates")
 
-# Creating an instance of Flask class
-app = Flask('__name__', template_folder="web_flask/templates")
 
 @app.route("/cities_by_states", strict_slashes=False)
-def state_city():
-    states = storage.all(State).values()
-    return render_template("8-cities_by_states.py", states=states)
+def get_cities_by_state():
+    """Displays an HTML page with a list of all states and related cities.
+
+    States/cities are sorted by name.
+    """
+    return render_template("8-cities_by_states.html",
+                           states=storage.all("State"))
+
 
 @app.teardown_appcontext
-def storage_close(exception):
+def close_session(exception):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
