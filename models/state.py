2cfc97e27d1805ea009bf021893ddecf3fba2b55
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-04-21 19:03:01 +0400

    fix error

--- models/state.py
+++ models/state.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
+from models.city import City
+from models import storage
 
 
 class State(BaseModel, Base):
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete', backref='state')
+    """State class"""
 
-    def __init__(self, *args, **kwargs):
-        """ Initialization of State instance """
-        super().__init__(*args, **kwargs)
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+
         @property
         def cities(self):
-            from models import storage
-            from models.city import City
             """Get a list of all related City objects."""
             city_list = []
             for city in list(storage.all(City).values()):
